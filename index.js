06d5f2dbad0a0f069125c4902319329da68a4210
Merajhosen <merajakhon7@gmail.com>
2022-05-20 09:34:21 +0600

    email service provider add

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express')
 const cors = require('cors');
 require('dotenv').config()
 const jwt = require('jsonwebtoken');
+var nodemailer = require('nodemailer');
+var sgTransport = require('nodemailer-sendgrid-transport');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000
@@ -11,6 +13,8 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ngwpr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 function varifyJWT(req, res, next) {
     const authHeader = req.headers.authorization
@@ -24,11 +28,45 @@ function varifyJWT(req, res, next) {
         }
         req.decoded = decoded
         next()
+
     });
 
 }
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ngwpr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+var emailSendOptions = {
+    auth: {
+        api_key: process.env.EMAIL_SENDER_KEY
+    }
+}
+const emailClient = nodemailer.createTransport(sgTransport(emailSendOptions));
+
+function sendAppointmentEmail(booking) {
+    const { patient, patientName, treatment, date, slot } = booking
+    var email = {
+        from: process.env.EMAIL_SENDER_ADDRESS,
+        to: patient,
+        subject: `Your Appointment for ${treatment} is on ${date} at ${slot} confrim`,
+        text: `Your Appointment for ${treatment} is on ${date} at ${slot} confrim`,
+        html: `
+        <div>
+        <h2>Hello ${patientName}</h2>
+        <p>Your Appointed ${treatment} is confrim</p>
+        <p>Looking forwared seeing you on ${date} at ${slot}</p>
+        </div>
+        `
+    };
+
+    emailClient.sendMail(email, function (err, info) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log('Message sent: ', info);
+        }
+    });
+
+}
+
 
 async function run() {
     try {
@@ -36,11 +74,25 @@ async function run() {
         const serviceCollection = client.db("doctors_portal").collection("services");
         const bookingCollection = client.db("doctors_portal").collection("bookings");
         const userCollection = client.db("doctors_portal").collection("users");
+        const doctorCollection = client.db("doctors_portal").collection("doctors");
 
+
+        //varify doctor 
+        const varifyAdmin = async (req, res, next) => {
+
+            const requester = req.decoded.email
+            const requestAccount = await userCollection.findOne({ email: requester })
+            if (requestAccount.role === "admin") {
+                next()
+            }
+            else {
+                res.status(403).send({ message: "Forbiden" })
+            }
+        }
 
         app.get('/service', async (req, res) => {
             const query = {}
-            const cursor = serviceCollection.find(query)
+            const cursor = serviceCollection.find(query).project({ name: 1 })
             const services = await cursor.toArray()
             res.send(services)
         })
@@ -69,22 +121,27 @@ async function run() {
             res.send({ admin: isAdmin })
         })
 
-        //make admin
-        app.put('/user/admin/:email', varifyJWT, async (req, res) => {
+        //manage doctor
+        app.get('/doctor', varifyJWT, varifyAdmin, async (req, res) => {
+            const doctors = await doctorCollection.find().toArray()
+            res.send(doctors)
+        })
+        app.delete('/doctor/:email', varifyJWT, varifyAdmin, async (req, res) => {
             const email = req.params.email
-            const requester = req.decoded.email
-            const requestAccount = await userCollection.findOne({ email: requester })
-            if (requestAccount.role === "admin") {
-                const filter = { email: email }
-                const updateDoc = {
-                    $set: { role: "admin" },
-                };
-                const result = await userCollection.updateOne(filter, updateDoc)
-                res.send(result)
-            }
-            else {
-                res.status(403).send({ message: "Forbiden" })
-            }
+            const filter = { email: email }
+            const result = await doctorCollection.deleteOne(filter)
+            res.send(result)
+        })
+
+        //make admin
+        app.put('/user/admin/:email', varifyJWT, varifyAdmin, async (req, res) => {
+            const email = req.params.email
+            const filter = { email: email }
+            const updateDoc = {
+                $set: { role: "admin" },
+            };
+            const result = await userCollection.updateOne(filter, updateDoc)
+            res.send(result)
 
         })
 
@@ -114,7 +171,7 @@ async function run() {
             }
 
         })
-        //booking
+
         app.post('/booking', async (req, res) => {
             const booking = req.body
             const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient }
@@ -123,7 +180,16 @@ async function run() {
                 return res.send({ success: false, booking: exists })
             }
             const result = await bookingCollection.insertOne(booking)
+            console.log("email is sending");
+            sendAppointmentEmail(booking)
             res.send({ success: true, result })
+        })
+
+        //insert doctor
+        app.post('/doctor', varifyJWT, varifyAdmin, async (req, res) => {
+            const doctor = req.body
+            const result = await doctorCollection.insertOne(doctor)
+            res.send(result)
         })
 
         app.get('/avilable', async (req, res) => {
